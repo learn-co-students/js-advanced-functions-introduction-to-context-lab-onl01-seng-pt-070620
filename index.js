c40c5cb6be4ca4f86a2560ee51b179c84674357b
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-01-31 16:18:58 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Your code here
+// let the challenge begin!!!
+
+function createEmployeeRecord(first_name, family_name, title, payPerHour) {
+    const employeeProfile = Object.assign({}, {
+        firstName: first_name,
+        familyName: family_name,
+        title: title,
+        payPerHour: payPerHour 
+    });
+    let timeInEvents = [];
+    let timeOutEvents = [];
+}
+
+function createEmployeeRecords(arrays) {
+    let newArr = arrays.map(accumulator, currentValue) => accumulator + currentValue)
+}
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+
+}
+
+function createTimeOutEvent() {
+
+}
+
+function hoursWorkedOnDate() {
+    
+}
+
+function wagesEarnedOnDate() {
+
+}
+
+function allWagesFor() {
+
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+
+}
+
+function calculatePayroll(employeeRecord) {
+    
+}
